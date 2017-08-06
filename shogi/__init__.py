3407ff7701c78ceba63098dcde5ab6e965baec6d
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-08-06 13:03:06 +0900

    v1.0.3

--- shogi/__init__.py
+++ shogi/__init__.py
@@ -21,7 +21,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Tasuku SUENAGA a.k.a. gunyarakun'
 __email__ = 'tasuku-s-github@titech.ac'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 import collections
 
