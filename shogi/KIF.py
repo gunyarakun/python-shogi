97b323cdb92338e970f82f45c7f8ccc531784982
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2021-02-27 22:46:12 +0100

    added: complete custom initial board from kif file - added support for custom initial boards in kif files

--- shogi/KIF.py
+++ shogi/KIF.py
@@ -120,22 +120,44 @@ class Parser:
         return line_sfen
 
     @staticmethod
+    def complete_custom_sfen(board, pieces_in_hand, turn):
+        if turn == shogi.BLACK:
+            turn_str = 'b'
+        else:
+            turn_str = 'w'
+
+        # add whos turn it is
+        sfen = ''.join((board, ' ', turn_str, ' '))
+
+        # if there are pieces in the hand
+        if sum(pieces_in_hand[shogi.BLACK].values()) + \
+           sum(pieces_in_hand[shogi.WHITE].values()):
+            for key, quantity in pieces_in_hand[shogi.BLACK].items():
+                piece = shogi.PIECE_SYMBOLS[key].upper()
+                if quantity > 1:
+                    sfen = ''.join((sfen, str(quantity), piece))
+                elif quantity == 1:
+                    sfen = ''.join((sfen, piece))
+
+            for key, quantity in pieces_in_hand[shogi.WHITE].items():
+                piece = shogi.PIECE_SYMBOLS[key].lower()
+                sfen = ''.join((sfen, str(quantity), piece))
+        else:
+            sfen = ''.join((sfen, '-'))
+
+        # add the initial move number
+        sfen = ''.join((sfen, ' 1'))
+
+        return sfen
+
+    @staticmethod
     def parse_move_str(line, last_to_square):
         # Normalize king/promoted kanji
-<<<<<<< HEAD
         line = line.replace('王', '玉')
         line = line.replace('竜', '龍')
         line = line.replace('成銀', '全')
         line = line.replace('成桂', '圭')
         line = line.replace('成香', '杏')
-=======
-        line = line.replace('\u738b', '\u7389')
-        line = line.replace('\u7adc', '\u9f8d')
-        line = line.replace('\u6210\u9280', '\u5168')
-        line = line.replace('\u6210\u6842', '\u572d')
-        line = line.replace('\u6210\u9999', '\u674f')
-        line = line.replace('+', '')
->>>>>>> fixed: problems with kif importer when multiple move branches exist
 
         m = Parser.MOVE_RE.match(line)
         if m:
@@ -201,27 +223,10 @@ class Parser:
         for line in kif_str.split('\n'):
             if len(line) == 0 or line[0] == "*":
                 pass
-<<<<<<< HEAD
             elif '：' in line:
                 (key, value) = line.split('：', 1)
                 value = value.rstrip('　')
                 if key == '先手' or key == '下手': # sente or shitate
-=======
-            elif line == '+---------------------------+':
-                if custom_sfen:
-                    custom_sfen = False
-                    # remove last forward slash
-                    sfen = sfen[:-1]
-                else:
-                    custom_sfen = True
-                    sfen = ''
-            elif custom_sfen:
-                sfen = ''.join((sfen, Parser.parse_board_line(line), '/'))
-            elif '\uff1a' in line:
-                (key, value) = line.split('\uff1a', 1)
-                value = value.rstrip('\u3000')
-                if key == '\u5148\u624b' or key == '\u4e0b\u624b': # sente or shitate
->>>>>>> fixed: problems with kif importer when multiple move branches exist
                     # Blacks's name
                     names[shogi.BLACK] = value
                 elif key == '後手' or key == '上手': # gote or uwate
@@ -239,13 +244,7 @@ class Parser:
                     sfen = Parser.HANDYCAP_SFENS[value]
                     if sfen is None:
                         raise ParserException('Cannot support handycap type "other"')
-<<<<<<< HEAD
             elif line == '後手番':
-=======
-                elif key == '\u5909\u5316':  # henka
-                    break  # TODO: add alternative move suggestions / branches
-            elif line == '\u5f8c\u624b\u756a':
->>>>>>> fixed: problems with kif importer when multiple move branches exist
                 # Current turn is white
                 current_turn = shogi.WHITE
             else:
@@ -280,6 +279,10 @@ class Parser:
                             # TODO: repetition of moves with continuous check
                             win = '-'
             line_no += 1
+
+        # if using a custom sfen
+        if len(sfen.split(' ')) == 1:
+            sfen = Parser.complete_custom_sfen(sfen, pieces_in_hand, current_turn)
 
         summary = {
             'names': names,
